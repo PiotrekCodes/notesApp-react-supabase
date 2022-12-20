a40c00e31033472392f28dd5ce7d01607647277f
Piotr Krawczyk <sztymperjan@gmail.com>
2022-12-20 19:50:27 +0000

    feat: updating notes function added

--- src/components/NotesCard.jsx
+++ src/components/NotesCard.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 const NotesCard = ({ note }) => {
   return (
     <div className="note-card">
       <h3>{note.title}</h3>
       <p>{note.content}</p>
+      <div className="buttons">
+        <Link to={"/" + note.id}>
+          <i className="material-icons">edit</i>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,9 +1,83 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import supabase from "../config/supabaseClient";
+
 const Update = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [formError, setFormError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !content) {
+      setFormError("Please fill in all fields correctly.");
+      return;
+    }
+
+    const { data, error } = await supabase
+      .from("notes")
+      .update({ title, content })
+      .eq("id", id)
+      .select();
+
+    if (error) {
+      console.log(error);
+      setFormError("Please fill in all fields correctly.");
+    }
+
+    if (data) {
+      setFormError(null);
+      navigate("/");
+    }
+  };
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const { data, error } = await supabase
+        .from("notes")
+        .select()
+        .eq("id", id)
+        .single();
+
+      if (error) {
+        navigate("/", { replace: true });
+      }
+
+      if (data) {
+        setTitle(data.title);
+        setContent(data.content);
+      }
+    };
+    fetchNotes();
+  }, [id, navigate]);
+
   return (
     <div className="page update">
-      <h2>Update</h2>
-    </div>
-  )
-}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-export default Update
+        <label htmlFor="method">Content:</label>
+        <textarea
+          id="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+
+        <button>Update a note</button>
+        {formError && <p className="error">{formError}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Update;
